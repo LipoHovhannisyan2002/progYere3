6fa0fb2a340fdffe3882cc128d3f93660331efef
user <lipo2002lenovo@gmail.com>
2019-06-04 18:57:21 +0400

    Programing 3 Yerevan

---
+++ Lesson3/intex.js
@@ -0,0 +1,24 @@
+// var express = require("express");
+// var app = express();
+
+// app.get("/", function(req, res){
+//    res.send("Hello world");
+// });
+
+// app.listen(3000, function(){
+//    console.log("Example is running on port 3000");
+// });
+//------------------------------------------------------
+
+// var express = require("express");
+// var app = express();
+
+// app.get("/", function(req, res){
+//    res.send("Hello Lip");
+// });
+
+// app.listen(3000, function(){
+//    console.log("Example is running on port 3000");
+// });
+//-----------------------------------------------------
+
