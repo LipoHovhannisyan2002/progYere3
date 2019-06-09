408aaa064bf287c4c10722eda261af9b8419b75e
LipoHovhannisyan2002 <lipo2002lenovo@gmail.com>
2019-06-09 13:56:16 +0400

    Lipo

--- Lesson3/intex.js
+++ Lesson3/intex.js
@@ -20,5 +20,37 @@
 // app.listen(3000, function(){
 //    console.log("Example is running on port 3000");
 // });
-//-----------------------------------------------------
+//----------------------------------------------------- 
+// var os = require("os");
+// var message = "The platform is ";
 
+// function main(){
+//    console.log(message + os.platform());
+// }
+// main();
+// ---------------------------------------------------------
+// var express = require("express");
+// var app = express();
+
+// app.get("/", function(req, res){
+//    res.send("<h1>EROR404</h1>");
+// });
+
+// app.get("/name/:search", function(req, res){
+//    var name = req.params.name;
+//    res.redirect('google.com/search?q=search value');
+// });
+// app.listen(3000, function(){
+//    console.log("Example is running on port 3000");
+// });
+// -----------------------------------------------------------
+
+// var routes = require('./routes');
+// var prod_routes = require('./routes_prod');
+// var env = process.env.NODE_ENV || 'production';
+// if ('google.com/search?q=search value' == env) {
+//     app.use('search', prod_routes);
+// } else {
+//     app.use('/', routes);
+// }
+//-------------------------------------------------------------
